dd19b3a1fa624bb43b64afee5325484a730259c3
RamssesArenas <l15200119@pachuca.tecnm.mx>
2021-04-03 13:34:36 -0600

    46

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+import Header from './commponents/Header'
+import Footer from './commponents/Footer'
+import Producto from './commponents/Producto'
+
+function App() {
+
+  //Crear listado de productos
+  const[productos,guardarProductos] = useState([
+    {id: 1,nombre: 'camisa 1', precio: 50},
+    {id: 2,nombre: 'camisa 2', precio: 60},
+    {id: 3,nombre: 'camisa 3', precio: 70},
+    {id: 5,nombre: 'camisa 4', precio: 30},
+  ]);
+  //State para carrito de compras
+  const [carrito, agregarProducto] = useState([]);
+  //Obtener la fecha
+  const fecha = new Date().getFullYear();
+  return (
+    <div className="App">
+      <Header
+        titulo='Tienda Virtual'
+      />
+      <h1>Lista de productos</h1>
+      {productos.map(producto =>(
+        <Producto
+          key = {producto.id}
+          producto={producto}
+          productos={productos}
+          carrito={carrito}
+          agregarProducto={agregarProducto}
+        /> 
+      ))}
+      <Footer
+        fecha={fecha}
+      />
+   </div>
+  );
+}
+
+export default App;
